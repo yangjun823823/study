133c11105a5ccddb73e1b43ab6f7a6c0cb92ea3a
yangjun <yangjun@unittec.com>
2019-10-29 14:22:45 +0800

    数组和链表-翻转单链表（其它代码）

--- data structure and algorithm/数组和链表/java/翻转单链表.java
+++ data structure and algorithm/数组和链表/java/翻转单链表.java
@@ -21,4 +21,18 @@ class Solution {
         curr.next = pre;
         return curr;
     }
+	
+	
+	public ListNode reverseList(ListNode head) {
+		ListNode prev = null;
+		ListNode curr = head;
+		while (curr != null) {
+			ListNode nextTemp = curr.next;
+			curr.next = prev;
+			prev = curr;
+			curr = nextTemp;
+		}
+		return prev;
+	}
 }
+
