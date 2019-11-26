99297dc84e4f507127e40920da04810e4a9268e0
yj <670483933@qq.com>
2019-11-26 20:48:14 +0800

    滑动窗口最大值.java --更改

--- data structure and algorithm/栈、队列和递归/java/滑动窗口最大值.java
+++ data structure and algorithm/栈、队列和递归/java/滑动窗口最大值.java
@@ -1,35 +1,35 @@
-class Solution {
-    //若队列不满，待入队元素 >= 队尾元素,队尾出队，循环...队尾入队
-    //若队列不满，待入队元素 < 队尾元素,队尾入队
-    //若队列满，队首出队,走队列不满逻辑
+public class Solution {
+    public static void main(String[] args){
+        int[] nums ={1,3,1,2,0,5};
+        int k = 3;
+		int[] result =  maxSlidingWindow(nums,k);
+    }
+
+    /**O(n)时间复杂度，维护一个双端队列，队首元素始终是最大值
+     * 算法：对要进队列的数，从队尾开始，若该数>=队尾元素便删除队尾元素，循环，然后入队；若该数<队尾元素便直接入队
+     * 注意：队列满时要移除队首元素；数组可能为空
+     * @param nums
+     * @param k
+     * @return
+     */
     public static int[] maxSlidingWindow(int[] nums, int k) {
-        Queue<Integer> queue = new LinkedList<Integer>();
-        if(nums.length == 0){
+        if (nums.length == 0){
             return new int[0];
         }
+        Deque<Integer> dequeue = new ArrayDeque<>();
         int[] result = new int[nums.length-k+1];
         int index = 0;
-        int i = 0;
-        for (; i < k; i++){
-            while (queue.size() > 0 &&  nums[i] >= queue.element()){
-                queue.poll();
+        for (int i = 0; i < nums.length; i++){
+            if(!dequeue.isEmpty() && i - dequeue.getFirst() == k){
+                dequeue.removeFirst();
             }
-            queue.offer(nums[i]);
-        }
-        result[index++] = queue.element();
-        for (; i < nums.length; i++){
-            if(queue.size() < k){//队列不满
-                while (queue.size() > 0 &&  nums[i] >= queue.element()){
-                    queue.poll();
-                }
-            }else{//队列满了
-                queue.poll();
-                while (queue.size() > 0 &&  nums[i] >= queue.element()){
-                    queue.poll();
-                }
+            while (!dequeue.isEmpty() && nums[i] >= nums[dequeue.getLast()]){
+                dequeue.removeLast();
             }
-            queue.offer(nums[i]);
-            result[index++] = queue.element();
+            dequeue.addLast(i);
+            if(i >= k - 1){
+                result[index++] = nums[dequeue.getFirst()];
+            }
         }
         return result;
     }
